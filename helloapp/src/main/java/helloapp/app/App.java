33885e87d7a7e94dea181f2c003170b30eab19d0
Princemax <princemax@PRINCE-MAXWELL.>
2025-01-21 14:24:51 +0100

    completed maven project

--- helloapp/src/main/java/helloapp/app/App.java
+++ helloapp/src/main/java/helloapp/app/App.java
@@ -5,6 +5,6 @@ package helloapp.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! It's Maven time!!");
     }
 }
